9889a76bbe94ae419d150c132d1fb9d7fe943610
HM15-Coder <75205390+HM15-Coder@users.noreply.github.com>
2020-12-06 16:01:45 +0530

    Add files via upload

---
+++ fruit.js
@@ -0,0 +1,56 @@
+img ="";
+status= "";
+objects = [];
+function preload()
+{
+  img = loadImage("fruit_basket_image.jpg");
+}
+function setup()
+{
+  canvas = createCanvas(650,450);
+  canvas.position(380,450);
+  canvas.center();
+  objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+  document.getElementById("status").innerHTML= "Status : Detecting Objects";
+}
+function draw()
+{
+   image(img,0,0,650,450);
+
+    if(status != ""){
+
+      r = random(255);
+      g = random(255);
+      b = random(255);
+
+      objectDetector.detect(img,gotResult);
+      
+
+      for(i = 0; i < objects.length; i++)
+      {
+        document.getElementById("status").innerHTML = "Status: Object Detected";
+        percent = floor(objects[i].confidence * 100);
+        fill(r,g,b);
+        text(objects[i].label + "" + percent + "%" , objects[i].x+15, objects[i].y+15);
+        noFill();
+        stroke(r,g,b);
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+      }
+    }
+}
+function modelLoaded()
+{
+  console.log("Model Loaded!");
+  status = true;
+  objectDetector.detect(img,gotResult);
+}
+function gotResult(error,results)
+{
+  if(error){
+    console.error(error);
+  }
+  else{
+    console.log(results);
+    objects = results;
+  }
+}
---
+++ main.js
@@ -0,0 +1,24 @@
+function next_page1()
+{
+    window.location.assign("bedroom.html");
+}
+function next_page2()
+{
+    window.location.assign("tv_ac.html");
+}
+function next_page3()
+{ 
+    window.location.assign("desk.html");
+}
+function next_page4()
+{
+    window.location.assign("bottle.html");
+}
+function next_page5()
+{
+    window.location.assign("fruit_basket.html");
+}
+function back()
+{
+    window.location.assign("index.html");
+}
